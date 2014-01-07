76f5ef4f4a98430fccec5557f9dcdd8794c64291
kuldeep-iiil <kuldeep_it2007@yahoo.co.in>
2014-01-07 13:04:41 +0530

    added changes for reported issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ NicksListProj::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
-  port:                 587,
+  port:                 465,
   domain:               'gmail.com',
   user_name:            'kuldeepc.iiil@gmail.com',
   password:             'kuldeep1#',
