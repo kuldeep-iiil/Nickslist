3689cdb42a39de7f9342fb69ba5820ce7a16ed97
kuldeep-iiil <kuldeep_it2007@yahoo.co.in>
2014-01-07 14:31:17 +0530

    added changes for reported issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,11 +29,13 @@ NicksListProj::Application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:              'email-smtp.us-east-1.amazonaws.com',
-  port:                 25,
-  user_name:            'AKIAJHQ6XRQI52P6WWMQ',
-  password:             'Al00Gje0xx9EOGyiyJ3p/khp7AkGQygmWqGqxa/VNXgA',
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'kuldeepc.iiil@gmail.com',
+  password:             'kuldeep1#',
   authentication:       'login',
+  ssl:                  true,
   enable_starttls_auto: true  }
   
 end
