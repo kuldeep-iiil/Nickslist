4770b3e7e21fb2f06523c7b7b97c5812ea749445
kuldeep-iiil <kuldeep_it2007@yahoo.co.in>
2014-01-16 19:37:21 +0530

    Made changes for change in review functionality

--- app/controllers/authorize_payment_controller.rb
+++ app/controllers/authorize_payment_controller.rb
@@ -93,11 +93,11 @@ class AuthorizePaymentController < ApplicationController
         @userEmail = @subscribedUser.EmailID
         @amount = @userPaymentDetails.TransactionAmount
           
-        mail_to_admin = UserMailer.NewUserNotification(@userfirstName, @userlastName, @useremail, @invoiceNumber, @amount)
-        mail_to_admin.deliver 
+        #mail_to_admin = UserMailer.NewUserNotification(@userfirstName, @userlastName, @useremail, @invoiceNumber, @amount)
+        #mail_to_admin.deliver 
           
-        mail_to_user = UserMailer.WelcomeUser(@userfirstName, @userlastName, @useremail, @invoiceNumber, @amount)
-        mail_to_user.deliver        
+        #mail_to_user = UserMailer.WelcomeUser(@userfirstName, @userlastName, @useremail, @invoiceNumber, @amount)
+        #mail_to_user.deliver        
                 
       end
       render :text => sim_response.direct_post_reply(AUTHORIZE_NET_CONFIG['receipt_url'] + '?in=' + @invoiceNumber)
